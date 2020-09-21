c0c4b46a7d780b7ecdc9e225241cc1da1a8fc70d
medhatmebed <medhat.mebed@gmail.com>
2020-09-20 20:58:53 -0500

    finish the base design

---
+++ app/src/main/java/com/example/instagramcloneproject/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.instagramcloneproject
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.instagramcloneproject.fragments.HomeFragment
+import com.example.instagramcloneproject.fragments.NotificationsFragment
+import com.example.instagramcloneproject.fragments.ProfileFragment
+import com.example.instagramcloneproject.fragments.SearchFragment
+
+class MainActivity : AppCompatActivity() {
+
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.nav_home -> {
+                moveToFragment(HomeFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_search -> {
+                moveToFragment(SearchFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_add_post -> {
+                item.isChecked = false
+
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_notifications -> {
+                moveToFragment(NotificationsFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_profile -> {
+                moveToFragment(ProfileFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+
+        moveToFragment(HomeFragment())
+    }
+
+
+    private fun moveToFragment(fragment: Fragment)
+    {
+        val fragmentTrans = supportFragmentManager.beginTransaction()
+        fragmentTrans.replace(R.id.fragment_container, fragment)
+        fragmentTrans.commit()
+    }
+}
